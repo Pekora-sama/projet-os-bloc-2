6162c4ab309ba0504cc0c5e83cd91b0c260a1ea4
Christopher Fauconnier <c.fauconnier@students.ephec.be>
2020-12-31 10:57:14 +0100

    écriture dans fichier texte
     correction de petites erreurs (commandes non-nécessaires et appel de fonction vide) + ajout de la création de fichier texte

--- cirquit.c
+++ cirquit.c
@@ -2,128 +2,237 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <sys/sem.h>
+
 #include "voiture.h"
 #include "rand.h"
 
+#define MAXFORK 20
 
-double* rouler(){
-    //int tid = pthread_self();
-    srand(time(NULL)^ getpid()); // graine, elle doit être dans les fils afin de prendre leurs pid unique à chaques fois
+int mesVoitures[20] = {44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
 
-    double temps[2]; // creation du tableau local
-    maVoiture test;
-    double S1,S2,S3;
-    test.S1 = 45.00;
-    test.S2 = 45.00;
-    test.S3 = 45.00;
+/*=======================================================
+Création des fonctions Sémaphore
+=========================================================*/
+void lancerCourse(int nbreVoiture,double raceTime,struct maVoiture pilotes[20]){
+    int i ;
+    int shmId;
+    int semId;
+    struct sembuf shmbuffer;
+    int nbLecteur;
+    //key_t key = ftok("/dev/null", 7);
+    struct maVoiture *circuit; //Structure qui stocke les voitures actuellement en piste
+    shmId = shmget(IPC_PRIVATE, 20*sizeof(struct maVoiture), IPC_CREAT|0666);
+    if(shmId == -1){ //Erreur lors de la creation de la memoire partagee
+        printf("Erreur lors de la creation de la mem partagee shmId = -1 ");
+        exit(1);
+    }
+    circuit = shmat(shmId,0,0);
+    if(circuit == (struct voiture*)-1){ //Erreur lors de l'acces a la memoire partagee
+        printf("Erreur shmat = -1");
+        exit(1);
+    }
+    void initSem(){ //Initalise le semaphore
+        //key_t semCle;
+        semId = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+      /*  if( semCle < 0 )
+        {
+            printf("Erreur semid\n");
+            exit(0);
+        }*/
+        semctl(semId, 0, SETVAL, 1);
+        semctl(semId, 1, SETVAL, 1);
+    }
+    void wait(int i){ //Met le thread en attente
+        shmbuffer.sem_num = i;
+        shmbuffer.sem_op = -1;
+        shmbuffer.sem_flg = SEM_UNDO;
+        semop(semId, &shmbuffer, 1);
+    }
+    void post(int i){ //Enleve le thread de la liste d'attente
+        shmbuffer.sem_num = i;
+        shmbuffer.sem_op = 1;
+        shmbuffer.sem_flg = SEM_UNDO;
+        semop(semId, &shmbuffer, 1);
+    }
+    initSem();
+
+    //==============Sémaphores + mémoire partagée + fork
+
+    int pid[MAXFORK];
+    int k,l;
+    int fini;
     int compteur = 0 ;
     double tourComplet = 45*3;
     double tempsAuStand ;
-
-    for(int i=0; i<NBR_TOURS; i++){  //permet de lancer la fonction x fois
-
+    double S1,S2,S3;
 
 
-        if(stand(TAUX_DE_STAND)==1){
-            tempsAuStand =tempsStand();
-            //printf("temps au stand %f \n",tempsAuStand);
+    for(k=0; k < MAXFORK ; k++){
+
+        pid[k] = fork() ;
+        if(pid[k]== -1){
+            printf("Impossible de créer un fils (%d)\n",k);
         }
-        else if (out(CRASH)==1){
+        else if(pid[k]==0){ // FILS
+            srand(time(NULL)^ getpid());                // graine, elle doit être dans les fils afin de prendre leurs pid unique à chaques fois
 
-            test.tempsTotal = tourComplet;
-            temps[0]=test.S1 ;
-            temps[1]=test.S2 ;
-            temps[2]=test.S3 ;
-            printf("out \n");
-            return temps ;
+            circuit = shmat(shmId,0,0);
+
+            if(circuit == (struct maVoiture*)-1){       //Erreur lors de l'acces a la memoire partagee
+                printf("Erreur shmat = -1");
+                exit(1);
+            }
+//=================================================TEST!!!!!=====================
+            circuit[k].out = 0;                         // met la voiture à crash = 0 => elle roule
+            circuit[k].numero = mesVoitures[k];      // donne l'id de la voiture
+            circuit[k].stand = 0;                       //met la voiture à stand = 0 => pas au stand
+            circuit[k].tempsTotal = 0;                  //défini le temps total à 0 (début de la course)
+            circuit[k].meilleurTemps = 0.0;             //défini le meilleur temps d'un tour à 0
+            circuit[k].meilleurTemps = 999.9;           // défini le meilleur temps à 999
+
+            while(circuit[k].out == 0 && circuit[k].tempsTotal < raceTime){
+                tempsAuStand = 0.0 ;                    // défini le temps passé au stand à 0
+
+                S1 = printRandoms() ;
+                S2 = printRandoms() ;                   //enregistre un temps de secteur
+                S3 = printRandoms() ;
+
+                if(stand(TAUX_DE_STAND)==1){            //génère un nombre alléatoire, si il est = à 1, la voiture va au stand
+                    tempsAuStand =tempsStand();         //gènère le temps en secondes que la voiture va passer au stand
+                    //printf("temps au stand %f \n",tempsAuStand);
+                }
+                else if (out(CRASH)==1){                //génère un nombre aléatoire, si il est = à 1, la voiture se crash
+                    wait(1);
+                    circuit[k].out = 1 ;
+                    circuit[k].S1 = S1;
+                    circuit[k].S2 = S2 ;
+                    circuit[k].S3 = S3;
+                    printf("La voiture %d est out.\n",circuit[k].numero);
+                    post(1);
+                    exit(0);
+                }
+
+               // printf("random ?%f \n",S1);
+
+                circuit[k].tempsTotal +=S1+S2+S3 ;                   // calcule le temps total, temps depuis le quel les voitures roules
+                //printf("Temps total : %f\n",circuit[k].tempsTotal);
+                if((S1+S2+S3) < circuit[k].meilleurTemps){          // si le dernier tour est le tour le plus rapide
+                    wait(2);
+                    circuit[k].meilleurTemps = S1+S2+S3 +tempsAuStand;     //enregistre le meilleur temps
+                    circuit[k].S1 = S1;
+                    circuit[k].S2 = S2;                                     //enregistre les données de S1, S2, S3
+                    //printf("temps au S2 %f \n",circuit[k].S2);
+                    circuit[k].S3 = S3;
+
+                    post(2);
+
+                }
+                //printf("Fils %2d (PID=%d): Active\n",k,getpid());
+
+            }
+            //printf("Temps total : %f\n",circuit[k].tempsTotal);
+            printf("Meilleur Temps de %d: %f\n",circuit[k].numero,circuit[k].meilleurTemps);
+
+            exit(0);
+            fflush(stdout);
         }
-        S1 = printRandoms() ;
-       // printf("%f \n",S1);
 
-        if (test.S1 > S1) {     //permet d'enregistrer le meilleur temps
-            test.S1 = S1;
+        else {
+            //printf("Pere : Activation du fils %2d\n", i);
+            fflush(stdout);
+           //printf("Père : Fin des activations\nAttente ...\n");
+
+            for(i=0;i<MAXFORK;i++){
+              if (pid[i]>0) {
+                if (waitpid(pid[i],NULL,WNOHANG)==0) {
+                  //printf("Père: fin du fils %2d (PID=%d)\n", i, pid[i]);
+                  fflush(stdout);
+                  pid[i]=0;
+                }
+              } else {
+                fini=0;
+              }
+            }
 
 
-        }
 
-        S2 = printRandoms() ;
-        if (test.S2 > S2) {
-            test.S2 = S2;
-        }
+          //printf("Fin de tous les fils.");
+          fflush(stdout);
+                }
 
-        S3 = printRandoms() ;
-        if (test.S3 > S3) {
-            test.S3 = S3;
-        }
 
-        if((S1+S2+S3) < tourComplet){
-            tourComplet = S1+S2+S3 +tempsAuStand;
-        }
+
 
     }
+    memcpy(pilotes,circuit,20*sizeof(struct maVoiture)); //permet de faire la copie en mémoire
 
-
-
-    test.tempsTotal = tourComplet;
-    temps[0]=test.S1 ;
-    temps[1]=test.S2 ;
-    temps[2]=test.S3 ;
-    printf ( "meilleur tour %f\n", test.tempsTotal);
-    //sem_wait(&semaphore);
-   // printf ( "secteur 1: %f\n", test.S1);
-    /*sleep(1);
-    sem_post(&semaphore);*/
-    //printf ( "secteur 2: %f\n", test.S2);
-    //printf ( "secteur 3: %f\n", test.S3);
-    //printf ( "le temps total est de : %f secondes\n", test.tempsTotal);
-    return temps;
 }
 
 
+//Creer les fichiers pour sauvegarder les resultats
+int creationFichier(int nbreVoiture,struct maVoiture pilotes[20]){
+    FILE* fichier = NULL;
 
+    fichier = fopen("Essai.txt", "w+");
+    if (fichier != NULL){
+        //Debut de l'ecriture
+        fprintf(fichier,"|N°\t|S1\t\t|S2\t\t|S3\t\t|Tour\t\t|Best\t\t|PIT\t|OUT\t|\n");
+        fprintf(fichier,"\n");
 
-/* ==========================================
-
-Vendredi matin séance d'essais libre d'1h30 (P1)
-
-=============================================*/
-
-
-
-
-
-/* ==========================================
-
-Vendredi après midi séance d'essais libre d'1h30 (P2)
-
-=============================================*/
-
-
-
-
-/* ==========================================
-
-Samedi matin séance d'essais libre d'1h (P3)
-
-=============================================*/
-
-
-
-
-/* ==========================================
-
-Samedi après midi séance de qualification en 3 parties
-
-Q1, durée 18 minutes => élimine 5 voitures
-Q1, durée 15 minutes => élimine 5 voitures
-Q3, durée 12 minutes => classe les 10 dernières voitures
-
-=============================================*/
-
-
-/* ==========================================
-
-Dimanche après midi la course en elle même
-
-=============================================*/
-
+        for(int j = 0; j < nbreVoiture ; j++){
+            fprintf(fichier,"|%d\t",pilotes[j].numero); //Imprimme le N°
+            if (pilotes[j].S1 == 0){ //Imprime le temps S1
+                fprintf(fichier,"|NULL\n");
+            }
+            else{
+                fprintf(fichier,"|%.3f\n",pilotes[j].S1);
+            }
+            if (pilotes[j].S2 == 0){ //Imprime le temps S2
+                fprintf(fichier,"|NULL\n");
+            }
+            else{
+                fprintf(fichier,"|%.3f\n",pilotes[j].S2);
+            }
+            if (pilotes[j].S3 == 0){ //Imprime le temps S3
+                fprintf(fichier,"|NULL\n");
+            }
+            else{
+                fprintf(fichier,"|%.3f\t",pilotes[j].S3);
+            }
+            if (pilotes[j].tempsTotal == 0){ //Imprime le temps du tour
+                fprintf(fichier,"|NULL\t\t");
+            }
+            else if(pilotes[j].tempsTotal<100.000){
+                fprintf(fichier,"|%.3f\t\t",pilotes[j].tempsTotal);
+            }
+            else{
+                fprintf(fichier,"|%.3f\t",pilotes[j].tempsTotal);
+            }
+            if (pilotes[j].meilleurTemps < 100.000){ //Imprimme le meilleur temps
+                fprintf(fichier,"|%.3f\t\t",pilotes[j].meilleurTemps);
+            }
+            else{
+                fprintf(fichier,"|%.3f\t",pilotes[j].meilleurTemps);
+            }
+            if(pilotes[j].stand != 0){ //Imprime le nombre de pit du pilote
+                fprintf(fichier,"|%d\t",pilotes[j].stand);
+            }
+            else{
+                fprintf(fichier,"|0\t");
+            }
+            if(pilotes[j].out == 1){ //Imprimme si le pilote est out
+                fprintf(fichier,"|X\t|\n");
+            }
+            else{
+            fprintf(fichier,"|\t|\n");
+            }
+        }//Fin ecriture
+    fclose(fichier);
+    }
+    return 0;
+}
--- main.c
+++ main.c
@@ -2,146 +2,92 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <semaphore.h>
-#include <pthread.h>
-#include <errno.h>
-#include "rand.h"
+
 #include "voiture.h"
 #include "cirquit.h"
-#define MAXFORK 20
-
-
-int sem_init(sem_t *sem, int pshared, unsigned int value);
-int sem_destroy(sem_t *sem);
-int sem_wait(sem_t *sem);
-int sem_post(sem_t *sem);
 
 
 
-int main(int argc, char**argv)
-{
 
-
+int main(int argc, char**argv){
+    //FILE* fichier = NULL;
+    int numero[20] = {44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
     maVoiture voitureEnMem[20]; //tableau de voitures
-    /* initialisation de la mémoire partagée */
-
-
-    struct shmid_ds shmbuffer;
-
-    /* Alloue le segment de mémoire partagée. */
-    int segment_id = shmget (IPC_PRIVATE, sizeof(maVoiture)*20, IPC_CREAT | 0666);
-
-    /* Attache le segment de mémoire partagée. */
-
-    maVoiture *shared_memory  = (maVoiture *) shmat(segment_id, NULL, 0);
-    printf ("memoire partagee attachee a l'adresse %p\n", shared_memory );
-
-    /* Écrit une chaîne dans le segment de mémoire partagée. */
-    sprintf (shared_memory , "Hello, world.");
-    memcpy(voitureEnMem, shared_memory , sizeof(maVoiture)*20);
-
-    /* affiche la chaine en mémoire partagée. */
-    printf ("%s\n", shared_memory);
-
-
-    //========================================
-
-   // pthread_t threads[MAXFORK];
-    //int sem_init(&semaphore, PTHREAD_PROCESS_SHARED, 1);
-    char a ="";
-    printf("lolol %d",strlen("semy"));
-
-
-
-
-
-
-
-
-    //========================================
-    int tableauResult[20][6] ;// creation du tableau global
-    //srand(time(NULL)^ getpid());//ATTENTION PAS TOUCHER --> la graine du générateur aléatoire doit être en dehors de la fct printRandom()
-
-    int tableau[20] = {44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
-    /*création d'un tableau de structure pour les 20 voitures
-    Si vous avez une meilleure idée n'hésitez pas à changer*/
-
-
-    int k = 0;
-    for(k=0; k<20; k++)
-    {
-        maVoiture tableau[k];       //Chaque élément du tableau devient une structure
-
+    for(int i = 0; i < 20 ; i++){ //Attribuer les id au tableau de voiture
+        voitureEnMem[i].numero = numero[i];
     }
-    //rouler();
+    //fichier = fopen("Essai.txt", "w+");
 
-
-
-
-    int pid[MAXFORK];
-    int i,j;
-    int fini;
-
-    for(i=0; i < MAXFORK ; i++){
-
-        pid[i] = fork() ;
-        if(pid[i]== -1){
-            printf("Impossible de créer un fils (%d)\n",i);
-        }
-        else if(pid[i]==0){ // FILS
-            //pthread_join(threads[i], NULL);
-            //faire les tours
-            sprintf (shared_memory , "je suis un des fils \n");
-            sleep(1);
-            //printf("Fils %2d (PID=%d): Activé\n",i,getpid());
-            fflush(stdout);
-            rouler();
-            exit(0);
-            fflush(stdout);
-
-        }
-        else {
-            //printf("Pere : Activation du fils %2d\n", i);
-            fflush(stdout);
-        }
-
+    //sleep(1);
+    lancerCourse(20,1000.0,voitureEnMem);
+    creationFichier(20,voitureEnMem);
+    sleep(1);
+    //fprintf(fichier,"|%.3f\t",voitureEnMem[0]);
+    /*
+    for(int i = 0 ; i <20 ; i++){
+        printf(" la voiture %d a un meilleur temps de %f\n",numero[i],voitureEnMem[i].meilleurTemps);
+        usleep(50000);
     }
+    */
 
-    //printf("Père : Fin des activations\nAttente ...\n");
-    fflush(stdout);
-    fini=0;
-    while (!fini) {
-    fini=1;
-    for(i=0;i<MAXFORK;i++){
-      if (pid[i]>0) {
-        if (waitpid(pid[i],NULL,WNOHANG)==0) {
-          //printf("Père: fin du fils %2d (PID=%d)\n", i, pid[i]);
-          fflush(stdout);
-          pid[i]=0;
-        }
-      } else {
-        fini=0;
-      }
-    }
-    }
+    sleep(1);
 
 
-  //printf("Fin de tous les fils.");
-  fflush(stdout);
+
+
+/* ==========================================
+
+Vendredi matin séance d'essais libre d'1h30 (P1)
+
+=============================================*/
+
+
+
+
+
+/* ==========================================
+
+Vendredi après midi séance d'essais libre d'1h30 (P2)
+
+=============================================*/
+
+
+
+
+/* ==========================================
+
+Samedi matin séance d'essais libre d'1h (P3)
+
+=============================================*/
+
+
+
+
+/* ==========================================
+
+Samedi après midi séance de qualification en 3 parties
+
+Q1, durée 18 minutes => élimine 5 voitures
+Q1, durée 15 minutes => élimine 5 voitures
+Q3, durée 12 minutes => classe les 10 dernières voitures
+
+=============================================*/
+
+
+/* ==========================================
+
+Dimanche après midi la course en elle même
+
+=============================================*/
 
 
 
     return 0;
+
+
+
+
 }
 
 
-
-
-
-/*=======================================================
-Création des fonctions Sémaphore
-=========================================================*/
 
